7a0d44138d2d720a42f15df90ddcd25c90409292
tansen2425 <dinislamtansen11@gmail.com>
2023-12-01 22:42:07 +0600

    done

--- src/Components/BodyRight.jsx
+++ src/Components/BodyRight.jsx
@@ -8,7 +8,7 @@ const BodyRight = () => {
     <div className= " bg-neutral-900 w-96 h-96 mt-16 shadow-zinc-700 shadow-lg  absolute" >
         </div>
         <div className=" absolute  w-64 h-screen ml-3 mb-28">
-            <img src="/public/static/images/tansen photo.png" alt="profile image" style={{height:445, width:400}} />
+            <img src=" /static/images/tansen photo.png" alt="profile image" style={{height:445, width:400}} />
         </div>
         
     </div>
